409b04b9e0bbe71c9c254f897f9e3ea131500de4
Delaney <villarreald810@gmail.com>
2021-07-08 16:53:34 +0000

    argument

--- RScript.r
+++ RScript.r
@@ -1,16 +1,19 @@
 library(ggplot2)
-read.csv("regrex1.csv")
+
+args <- commandArgs(trailingOnly = TRUE)
+
+read.csv(args[1])
 
 #add argument instead of regrex1.csv
-regrex1 <- read.csv("regrex1.csv")
+regrex1 <- read.csv(args[1])
 
 png("scatter.png")
 ggplot(regrex1, aes(x,y)) + geom_point()
+dev.off()
 
 #ggplot(regrex1, aes(x,y)) + geom_smooth(method ="lm", se = F)
 
 png("fit.png")
 ggplot(regrex1, aes(x,y)) + geom_point() + geom_smooth(method ="lm", se = F)
-
 dev.off()
 
